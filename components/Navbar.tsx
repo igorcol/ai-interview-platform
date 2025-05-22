cd44c7ea0256de6b754764e17985fa60905dfb10
Igor - NOTE_2 <igor.colombini@gmail.com>
2025-05-22 20:34:10 -0300

    fix classname

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,7 +5,7 @@ import React from "react";
 const Navbar = () => {
   return (
     <nav>
-      <Link href="/" className="flex items-cnter gap-2">
+      <Link href="/" className="flex items-center gap-2">
         <Image src="/logo.svg" alt="PrepWise Logo" width={38} height={32} />
         <h2 className="text-primary-100">PrepWise</h2>
       </Link>
